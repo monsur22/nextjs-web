276ede3052fde5bd81117e2784d68fdb9144b271
monsur22 <monsurahmedshafiq@gmail.com>
2021-11-30 22:08:42 +0600

    getServerSideProps complete

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,12 +1,16 @@
 import {useRouter} from 'next/router'
+import Link from 'next/link'
 
+const article = ({ article}) => {
 
-const article = () => {
-    const router = useRouter()
-    const {id} = router.query
+    // const router = useRouter()
+    // const {id} = router.query
     return (
         <div>
-            <h1>Article {id}</h1>
+            <h1> {article.title}</h1>
+            <p> {article.body}</p>
+            <br/>
+            <Link href="/">Go Back</Link>
         </div>
     )
 }
@@ -14,7 +18,7 @@ const article = () => {
 export const getServerSideProps = async (context) => {
 
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-    
+
     const article = await res.json()
     return{
         props: {
