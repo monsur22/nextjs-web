09e1819994486636878fc198d52ad78a18f67bee
monsur22 <monsurahmedshafiq@gmail.com>
2021-11-30 21:41:51 +0600

    update by id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,25 @@
+import {useRouter} from 'next/router'
+
 
 const article = () => {
+    const router = useRouter()
+    const {id} = router.query
     return (
         <div>
-            <h1>Article</h1>
+            <h1>Article {id}</h1>
         </div>
     )
 }
 
+export const getServerSideProps = async (context) => {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    
+    const article = await res.json()
+    return{
+        props: {
+            article
+        }
+    }
+}
 export default article
