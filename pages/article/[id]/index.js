21ebca0acd9bc35312617d5212c2a3e874cdee13
monsur22 <monsurahmedshafiq@gmail.com>
2021-11-30 22:21:51 +0600

    getStaticPaths complete

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -15,15 +15,31 @@ const article = ({ article}) => {
     )
 }
 
-export const getServerSideProps = async (context) => {
-
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+export const getStaticProps = async (context) => {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+    )
 
     const article = await res.json()
-    return{
+
+    return {
         props: {
-            article
-        }
+        article,
+        },
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+    return {
+        paths,
+        fallback: false,
     }
 }
 export default article
